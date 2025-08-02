103252470529503e8679358d4ae2a4c2fc5d74d0
HaceneHamza <hamza2023hacene@gmail.com>
2025-08-02 15:57:27 +0100

    before last

--- Frontend/EcoVision/src/components/TopSellers.jsx
+++ Frontend/EcoVision/src/components/TopSellers.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const TopSellers = () => {
+function TopSellers() {
   const [topSellers, setTopSellers] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -109,6 +109,6 @@ const TopSellers = () => {
       </div>
     </div>
   );
-};
+}
 
 export default TopSellers;
