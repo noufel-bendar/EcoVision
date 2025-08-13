2b151491148a2c70334cba60d64513e6fdfc7250
noufel-bendar <bendarnoufel@gmail.com>
2025-08-13 12:14:18 +0100

    Fix CORS for new Vercel frontend

--- Backend/config/settings.py
+++ Backend/config/settings.py
@@ -43,12 +43,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = False
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "https://eco-vision-j42t.vercel.app",
-    "https://eco-vision-swart.vercel.app",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "https://eco-vision-j42t.vercel.app",
+#     "https://eco-vision-swart.vercel.app",
+# ]
 CORS_ALLOW_HEADERS = [
     "authorization",
     "content-type",
